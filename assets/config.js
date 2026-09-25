cb547bbd73e4dcc51e06d720f57caac81d781d73
elmanitoisma <gtavinofilter@gmail.com>
2026-09-25 14:51:22 +0200

    Activar Google Analytics 4

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.CC_CONFIG = {
-  SITE_URL: "https://cuentas-claras-nvsblw3yw-los-cuajaos.vercel.app",
-  GA_MEASUREMENT_ID: ""
+  SITE_URL: "https://cuentas-claras-murex-delta.vercel.app",
+  GA_MEASUREMENT_ID: "G-V6BGJWR4B9"
 };
